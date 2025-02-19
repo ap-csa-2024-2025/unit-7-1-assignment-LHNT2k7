21a2a71b02cbcbb3f0c12de0209707636616d712
LHNT2k7 <ltri9573@gmail.com>
2025-02-19 10:10:44 -0800

    fcgcgugiygfh

--- Main.java
+++ Main.java
@@ -8,9 +8,15 @@ public class Main
     Scanner sc = new Scanner(System.in);
     String input = "";
 
+    ArrayList<String> words = new ArrayList<String>;
+
+
     while (!input.equals("STOP"))
     {
-      System.out.println("infinite loop! replace with your code");
+      System.out.println("In the loop. Enter a word");
+      input = sc.nextLine();
+      words.add(input);
+      System.out.println("The ArraList is now " + words);
     }
   }
 }
